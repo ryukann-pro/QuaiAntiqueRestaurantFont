efe663c27cc31c5db1c9707d8469c33755325677
ryukann <yoannlamouille@gmail.com>
2026-01-14 10:34:09 +0100

    validation mot de passe et ajout classe BS feedback sur l'html

--- js/auth/signup.js
+++ js/auth/signup.js
@@ -18,13 +18,32 @@ function validateForm() {
   const nomOk = validateRequired(nomInput);
   const prenomOk = validateRequired(prenomInput);
   const mailOK = validateMail(emailInput);
+  const passwordOk = validatePassword(passwordInput);
 
-  if(nomOk && prenomOk && mailOK){
+  if(nomOk && prenomOk && mailOK && passwordOk){
     btnValidation.disabled = false;
   }else{
     btnValidation.disabled = true;
   }
 }
+
+
+function validatePassword(input) {
+  //définir mon regex
+  const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_])[A-Za-z\d\W_]{8,}$/;
+  const passwordlUser = input.value;
+  if (passwordlUser.match(passwordRegex)) {
+    input.classList.add("is-valid");
+    input.classList.remove("is-invalid");
+    return true;
+  } else {
+    // c'est pas ok
+    input.classList.remove("is-valid");
+    input.classList.add("is-invalid");
+    return false;
+  }
+}
+
 
 function validateMail(input) {
   //définir mon regex
@@ -55,3 +74,5 @@ function validateRequired(input) {
     return false;
   }
 }
+
+
