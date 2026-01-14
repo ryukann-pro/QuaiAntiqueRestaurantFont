e605d1610006c59273982a5cfbe98c1601a059ae
ryukann <yoannlamouille@gmail.com>
2026-01-14 10:15:42 +0100

    vérification email + modifi button

--- js/auth/signup.js
+++ js/auth/signup.js
@@ -1,31 +1,57 @@
 //implémenter le JS de ma page
 
+const btnValidation = document.getElementById("btnValidationInscription");
 const nomInput = document.getElementById("nomInput");
 const prenomInput = document.getElementById("prenomInput");
 const emailInput = document.getElementById("emailInput");
 const passwordInput = document.getElementById("passwordInput");
 const validatePasswordInput = document.getElementById("validatePasswordInput");
 
-nomInput.addEventListener("keyup",validateForm);
-prenomInput.addEventListener("keyup",validateForm);
-emailInput.addEventListener("keyup",validateForm);
-passwordInput.addEventListener("keyup",validateForm);
-validatePasswordInput.addEventListener("keyup",validateForm);
+nomInput.addEventListener("keyup", validateForm);
+prenomInput.addEventListener("keyup", validateForm);
+emailInput.addEventListener("keyup", validateForm);
+passwordInput.addEventListener("keyup", validateForm);
+validatePasswordInput.addEventListener("keyup", validateForm);
 
 
-function validateForm(){
-  validateRequired(nomInput);
-  validateRequired(prenomInput);
+function validateForm() {
+  const nomOk = validateRequired(nomInput);
+  const prenomOk = validateRequired(prenomInput);
+  const mailOK = validateMail(emailInput);
+
+  if(nomOk && prenomOk && mailOK){
+    btnValidation.disabled = false;
+  }else{
+    btnValidation.disabled = true;
+  }
 }
 
-function validateRequired(input){
-  if(input.value != ""){
-    // c'est ok
+function validateMail(input) {
+  //définir mon regex
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  const mailUser = input.value;
+  if (mailUser.match(emailRegex)) {
     input.classList.add("is-valid");
     input.classList.remove("is-invalid");
-  }else{
+    return true;
+  } else {
     // c'est pas ok
     input.classList.remove("is-valid");
     input.classList.add("is-invalid");
+    return false;
+  }
+}
+
+function validateRequired(input) {
+  if (input.value != "") {
+    // c'est ok
+    input.classList.add("is-valid");
+    input.classList.remove("is-invalid");
+    return true;
+  } else {
+    // c'est pas ok
+    input.classList.remove("is-valid");
+    input.classList.add("is-invalid");
+    return false;
   }
 }
